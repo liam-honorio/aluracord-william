f4a703e2b312188ed0cd5ad84cde3f3be38be4b7
William <mhf.eng.dsg@gmail.com>
2022-01-25 12:47:57 -0300

    State, novas páginas e navegação SPA

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,9 @@
 import { Box, Button, Text, TextField, Image } from "@skynexui/components"
-
+import { useState } from "react";
+import { useRouter } from "next/router";
 import appConfig from "../config.json";
 
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: 'Open Sans', sans-serif;
-      }
-      /* App fit Height */ 
-      html, body, #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */ 
-    `}</style>
-  );
-}
+
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -48,11 +22,11 @@ function Titulo(props) {
 }
 
 export default function PaginaInicial() {
-  const username = 'peas';
+  const [username, setUsername] = useState('liam-honorio');
+  const route = useRouter();
 
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -79,6 +53,12 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit = {
+              function(event) {
+                event.preventDefault();
+                route.push('/chat')
+              }
+            }
             styleSheet={{
               display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
               width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -90,6 +70,12 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
+              onChange={
+                function (event) {
+                  const valor = event.target.value;
+                  setUsername(valor);
+                }
+              }
               fullWidth
               textFieldColors={{
                 neutral: {
